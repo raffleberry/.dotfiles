d26cc097089bc52cd12baa948c2b11f039952aee
raffleberry <raffleberry42@gmail.com>
2025-01-03 00:57:53 +0530

    script: remove & pin snap, install ff

---
+++ delsnap.sh
@@ -0,0 +1,19 @@
+sudo apt purge snapd -y
+sudo cat <<EOF | sudo tee /etc/apt/preferences.d/nosnap.pref
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
+rm -rf ~/snap
+sudo rm -rf /snap
+sudo rm -rf /var/snap
+sudo rm -rf /var/lib/snapd
+sudo cat <<EOF | sudo tee  /etc/apt/preferences.d/firefox-no-snap
+Package: firefox*
+Pin: release o=Ubuntu*
+Pin-Priority: -1
+EOF
+
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt update
+sudo apt install firefox -y
