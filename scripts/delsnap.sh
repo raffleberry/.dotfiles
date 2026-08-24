e40dc7e19333b935cee24ed860198bb5b0778954
raffleberry <raffleberry42@gmail.com>
2026-08-25 02:03:44 +0530

    scripts

--- scripts/delsnap.sh
+++ scripts/delsnap.sh
@@ -8,15 +8,31 @@ rm -rf ~/snap
 sudo rm -rf /snap
 sudo rm -rf /var/snap
 sudo rm -rf /var/lib/snapd
-sudo cat <<EOF | sudo tee  /etc/apt/preferences.d/firefox-no-snap
-Package: firefox*
-Pin: release o=Ubuntu*
-Pin-Priority: -1
+
+sudo apt update
+sudo apt install -y qbittorrent ffmpeg kubuntu-restricted-* vlc
+
+mkdir -p $HOME/.gnupg/
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+sudo tee /etc/apt/sources.list.d/mozilla.sources > /dev/null << EOF
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc
 EOF
 
-sudo add-apt-repository ppa:mozillateam/ppa
-sudo apt update
-sudo apt install -y qbittorrent ffmpeg kubuntu-restricted-* vlc  stow
-sudo apt install firefox -y
+sudo tee /etc/apt/preferences.d/mozilla > /dev/null << EOF
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+EOF
 
+sudo apt-get -y update
+sudo apt-get install -y firefox
 
+sudo apt purge -y elisa haruna kdeconnect kmahjongg kmines kpat ksudoku
+sudo apt autoremove -y
+sudo apt upgrade -y
