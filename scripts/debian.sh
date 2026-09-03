44f95849bb4f504b978b74ba2989c2bf537514e0
raffleberry <raffleberry42@gmail.com>
2026-09-03 21:08:32 +0530

    deb

---
+++ scripts/debian.sh
@@ -0,0 +1,8 @@
+sudo apt purge -y akregator anthy anthy-common dragonplayer goldendict-ng juk kaddressbook kdeconnect kmail konqueror mozc-utils-gui mozc-server && sudo apt autoremove -y
+
+sudo apt install desktop-file-utils -y
+
+sudo apt upgrade -y
+
+sudo apt install systemd-resolved systemd-timesyncd keepassxc vlc -y
+
